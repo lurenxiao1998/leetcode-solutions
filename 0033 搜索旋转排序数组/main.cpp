1e3505dcc5304b485d408050ad22010a11c432c3
lurenxiao1998 <1909015072@qq.com>
2020-12-04 14:06:09 +0800

    0033

---
+++ 0033 搜索旋转排序数组/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
