fad03880b75d46397e0d66486e37e7ced0212473
lurenxiao1998 <1909015072@qq.com>
2021-03-27 16:26:56 +0800

    c++ version

--- 0011 盛最多水的容器/main.cpp
+++ 0011 盛最多水的容器/main.cpp
@@ -1,14 +1,34 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
- 
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        for (vector<int>::iterator it = height.begin(); it != height.end() ; it++)
-        {
-            
+        auto li=height.begin(),ri=height.end()-1;
+        int maxSize = min(*li,*ri)*(ri-li);
+        int size=0;
+        int hli=0,hri=0;
+        while(ri>li){
+            hri=*ri;
+            hli=*li;
+            if( hli  > hri){
+
+                while( ri> li && *--ri > hri ){
+                }
+                size=min(*li,*ri)*(ri-li);
+                if(size>maxSize){
+                    maxSize=size;
+                }
+            }
+            else{
+                while( ri> li && *++li > hli ){
+                    cout<< "2:" << *li;
+                }
+                size=min(*li,*ri)*(ri-li);
+                if(size > maxSize){
+                    maxSize=size;
+                }
+            }
         }
-        
+        return maxSize;
     }
 };
