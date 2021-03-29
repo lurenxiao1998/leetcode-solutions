707eea39004a071af365f8a51c79f3d4f47eaaa7
lurenxiao1998 <1909015072@qq.com>
2021-03-29 16:39:37 +0800

    rewrite c++ version

--- 0029 两数相除/main.cpp
+++ 0029 两数相除/main.cpp
@@ -1,33 +1,22 @@
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
-  int divide(int dividend, int divisor) {
-    int sig = dividend >> 31 ^ divisor >> 31;
-    dividend = dividend < 0 ? dividend : -dividend;
-    divisor = divisor < 0 ? divisor : -divisor;
-    int r = 0;
-    int digit = 1;
-    int tmpdivisor = divisor;
-    while (dividend <= divisor) {
-      digit = 1;
-      int c=tmpdivisor + tmpdivisor;
-      while (c >= dividend && c < 0) {
-        tmpdivisor += tmpdivisor;
-        digit <<= 1;
-        c=tmpdivisor + tmpdivisor;
-      }
-      dividend -= tmpdivisor;
-      tmpdivisor = divisor;
-      r -= digit;
+    int divide(int dividend, int divisor) {
+        int pos = (dividend>>31) ^ ( divisor >>31);
+        int res=0;
+        int negdividend = dividend>0?-dividend:dividend;
+        int negdivisor = divisor>0?-divisor:divisor;
+        int t=-1;
+        int tmpdivisor = negdivisor;
+        while( negdividend <= negdivisor ){
+            t=-1;
+            tmpdivisor = negdivisor;
+            while( tmpdivisor >= INT_MIN/2 && tmpdivisor + tmpdivisor>negdividend ){
+                t=t+t;
+                tmpdivisor += tmpdivisor;
+            }
+            negdividend -= tmpdivisor;
+            res += t;
+        }
+        return pos==0?res<-INT_MAX?INT_MAX:-res:res;
     }
-    int v=sig<0? r : r==INT_MIN? INT_MAX:-r;
-    return sig<0? r : r==INT_MIN? INT_MAX:-r ;
-  }
 };
-int stringToInteger(string input) { return stoi(input); }
-
-int main() {
-  int ret = Solution().divide(-2147483648, 3);
-  return 0;
-}
