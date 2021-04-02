a6a9bad51d36cd7942c89011549cab8edd6cf42c
lurenxiao1998 <1909015072@qq.com>
2021-04-02 17:28:15 +0800

    0083 删除排序链表中的重复元素

---
+++ 0083 删除排序链表中的重复元素/main.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head== nullptr)
+            return nullptr;
+        ListNode* node=head;
+        while(node->next != nullptr){
+            if(node->next->val == node->val)
+                node->next=node->next->next;
+            else
+                node=node->next;
+        }
+        return head;
+    }
+};
