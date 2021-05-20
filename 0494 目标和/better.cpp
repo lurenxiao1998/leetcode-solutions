97bb03131b6be10f3a15e52de6853675d29bd1e8
lurenxiao1998 <1909015072@qq.com>
2021-05-20 11:10:24 +0800

    0494 动态规划 dp

---
+++ 0494 目标和/better.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int S) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(S>sum || (S+sum)&0x1 )
+            return 0;
+        int target = (S + sum)/2;
+        vector<int> dp(target+1,0);
+        dp[0]=1;
+        for(auto num:nums){
+            for(int i=target;i>=num;i--){
+                dp[i] = dp[i] + dp[i-num];
+            }
+        }
+        return dp[target];
+    }
+};
